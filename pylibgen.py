bc110e61110c66938f20c40a62f89e89b653f87d
Joshua Li <joshua.r.li.98@gmail.com>
2017-03-07 17:39:34 -0500

    metadata querying complete

--- pylibgen.py
+++ pylibgen.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 import requests
 
 MIRRORS = [
@@ -9,6 +10,23 @@ MIRRORS = [
 
 s_url = 'http://{}/search.php?req={}&res=100&column={}'
 d_url = 'http://{}/get.php?md5={}'
+q_url = 'http://{}/json.php?ids={}&fields={}'
+
+
+def get_metadata(mirror, ids, fields=[
+        'title', 'author', 'year', 'edition', 'pages',
+        'identifier', 'extension', 'filesize', 'md5'
+    ]):
+    '''Returns a list of JSON dicts each containing metadata field
+    values for each libgen book ID. Uses the unofficial libgen query
+    API to retrieve this information.
+    
+    The default fields are probably enough, but there are a LOT
+    more like openlibraryid, publisher, etc. To get all fields,
+    use fields=['*'].
+    '''
+    url = q_url.format(mirror, ','.join(ids), ','.join(fields))
+    return requests.get(url).json()
 
 
 def download(mirror, md5, dest='.'):
@@ -24,3 +42,10 @@ def download(mirror, md5, dest='.'):
     with open(os.path.join(dest, md5), 'wb') as f:
         for chunk in r.iter_content(1024):
             f.write(chunk)
+
+
+if __name__ == '__main__':
+    # Example usage.
+    j = get_metadata(MIRRORS[0], ids=['1', '2', '3'])
+    from pprint import pprint
+    pprint(j)
