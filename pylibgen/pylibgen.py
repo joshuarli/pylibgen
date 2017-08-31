d363f180fcae0303dc8ec372b7d26f1c70b1e7cb
camoverride <camoverride@gmail.com>
2017-08-30 18:29:43 -0700

    raise HTTPError instead of returning an empty list

--- pylibgen/pylibgen.py
+++ pylibgen/pylibgen.py
@@ -65,6 +65,8 @@ class Library(object):
             'ids': ','.join(ids),
             'fields': ','.join(fields),
         }).json()
+        if not res:
+            raise requests.HTTPError(400)
         return res if len(res) > 1 else res[0]
 
     def get_download_url(self, md5, enable_ads=False):
