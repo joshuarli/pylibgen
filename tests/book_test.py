653e1f2de22a049c20d10c7117a3d3c230c0d236
Joshua Li <josh@jrl.ninja>
2018-09-27 09:28:38 -0700

    reduce timeout on filehost checks and make xfail

--- tests/book_test.py
+++ tests/book_test.py
@@ -6,11 +6,14 @@ import pylibgen
 
 
 def check_url(url):
-    r = requests.head(url, allow_redirects=True, timeout=10)
+    r = requests.head(url, allow_redirects=True, timeout=3)
     r.raise_for_status()
 
 
 @pytest.mark.parametrize("fh", pylibgen.constants.FILEHOST_URLS)
 def test_filehosts(fh):
     b = pylibgen.Book(id=1421206, md5="1af2c71c1342e850e1e47013b06f9eb9")
-    check_url(b.get_url(fh))
+    try:
+        check_url(b.get_url(fh))
+    except requests.exceptions.ReadTimeout as e:
+        pytest.xfail(f"Attempt to reach filehost {fh} timed out.")
